b1a89842b9c27858b9f94085509543fe8b1f1027
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-15 20:57:02 +0200

    test rscam

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
+/*
+sudo apt-get install -y libv4l-dev
+ */
+
 use rpcamrs::rpcamrs::*;
+use std::fs;
+use std::io::Write;
 
 fn main() {
-    println!(
-        "{:?}",
-        single_capture("test.jpg", None, None, None).unwrap()
-    );
+    use rscam::{Camera, Config};
+
+    let mut camera = Camera::new("/dev/video0").unwrap();
+
+    camera
+        .start(&Config {
+            interval: (1, 30), // 30 fps.
+            resolution: (1280, 720),
+            format: b"MJPG",
+            ..Default::default()
+        })
+        .unwrap();
+
+    for i in 0..10 {
+        let frame = camera.capture().unwrap();
+        let mut file = fs::File::create(&format!("frame-{}.jpg", i)).unwrap();
+        file.write_all(&frame[..]).unwrap();
+    }
 }
