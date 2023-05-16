c28912d5a74e58f8932cd7715069ea9d6c70ce29
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-16 23:42:03 +0200

    test image

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 /*
-sudo apt-get install -y libv4l-dev
-sudo apt-get install v4l-utils
-sudo apt-get install gettext
-sudo apt-get install v4l2loopback-dkms
-sudo apt install -y libclang-dev
+sudo apt-get install -y libv4l-dev 1
+build realese
  */
 
-use rpcamrs::rpcamrs::*;
-use std::fs;
-use std::io::Write;
+use image::io::{Reader as ImageReader, Reader};
+use image::{save_buffer, ColorType, EncodableLayout, ImageBuffer, Pixel, RgbaImage};
+use std::io::{Cursor, Write};
+use std::path::Path;
+use std::time::{Duration, SystemTime};
+use std::{fs, thread};
 
 fn main() {
     use rscam::{Camera, Config};
@@ -23,10 +23,32 @@ fn main() {
             ..Default::default()
         })
         .unwrap();
+    thread::sleep(Duration::from_millis(1000));
 
-    for i in 0..10 {
+    for i in 0..1 {
         let frame = camera.capture().unwrap();
-        let mut file = fs::File::create(&format!("frame-{}.jpg", i)).unwrap();
-        file.write_all(&frame[..]).unwrap();
+        //let mut file = fs::File::create(&format!("frame-{}.jpg", i)).unwrap();
+        //file.write_all(&frame[..]).unwrap();
+
+        //println!("{:?}", frame);
+
+        let img2 = Reader::new(Cursor::new(&frame[..]))
+            .with_guessed_format()
+            .unwrap()
+            .decode()
+            .unwrap();
+
+        //let img = ImageReader::open("frame-0.jpg").unwrap().decode().unwrap();
+        println!("{:?}", (img2.width(), img2.height()));
+        println!("{}", img2.as_bytes().len());
+
+        save_buffer(
+            &Path::new("image.jpg"),
+            img2.as_bytes(),
+            img2.width(),
+            img2.height(),
+            ColorType::Rgb8,
+        )
+        .unwrap();
     }
 }
